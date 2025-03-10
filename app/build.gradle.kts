41ca2a189b6d61864bb43ff6d522e91198d4b55c
Fadejimi <adegbulugbefadejimi@gmail.com>
2025-03-10 10:54:24 -0500

    depencency 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
     implementation(libs.androidx.hilt.navigation.fragment)
+    implementation(libs.androidx.hilt.navigation.compose)
 
     // corountines
     implementation(libs.kotlinx.coroutines.android)
