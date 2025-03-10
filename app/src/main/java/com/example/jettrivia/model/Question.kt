b9e0d0e7c4245dfffc3ec3748992897605efbfdd
Fadejimi <adegbulugbefadejimi@gmail.com>
2025-03-10 11:19:25 -0500

    Set the application and model for UI

--- app/src/main/java/com/example/jettrivia/model/Question.kt
+++ app/src/main/java/com/example/jettrivia/model/Question.kt
@@ -1,4 +1,14 @@
 package com.example.jettrivia.model
 
-class Question {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "question_table")
+data class Question(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val question: String,
+    val category: String,
+    val answer: String,
+    val choices: List<String>
+)
